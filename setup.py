160401581aa1fb8a82f0114e6772a76a34353b9f
mishok06333 <112792073+mishok06333@users.noreply.github.com>
2025-04-25 20:42:51 +1000

    Modify GitHub Actions workflow. Add tests

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bmi-calculator",
+    version="1.0.0",
+    author="Your Name",
+    description="BMI Calculator Package",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+)
