23059e8654d0ead8b316ad2c9948a06144d988e8
mishok06333 <112792073+mishok06333@users.noreply.github.com>
2025-05-15 17:48:29 +1000

    Add documentation with Doxygen

---
+++ docs/html/bmi_8py.js
@@ -0,0 +1,7 @@
+var bmi_8py =
+[
+    [ "bmi.calculate_bmi", "bmi_8py.html#aac45ecd2513e9b637f51159653eee345", null ],
+    [ "bmi.calculate_ideal_weight", "bmi_8py.html#a930c585e8611ade5514d57ecde5830e6", null ],
+    [ "bmi.get_bmi_category", "bmi_8py.html#a17d650956b3df7cf7fd1d02d07b6bcd3", null ],
+    [ "bmi.validate_input", "bmi_8py.html#a49b4a2547e68734835ccfbc13fa5f14b", null ]
+];
---
+++ docs/html/navtreeindex0.js
@@ -0,0 +1,7 @@
+var NAVTREEINDEX0 =
+{
+"bmi_8py.html":[0,0,0],
+"files.html":[0,0],
+"index.html":[],
+"pages.html":[]
+};
